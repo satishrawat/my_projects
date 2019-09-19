c9ed61245c5a8226b9246169001872ba5650f5ae
satishrawat <39967722+satishrawat@users.noreply.github.com>
2019-09-19 23:50:46 +0530

    Update smoke.sh

--- smoke.sh
+++ smoke.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 code=$(curl -Is http://35.239.92.56:80 | head -n 1)
 echo 'this is my code' $code
-if [ "$code" = "HTTP/1.1 200 OK" ]; then
+if [ "code" = "HTTP/1.1 200 OK" ]; then
   echo "Website $url is online."
 else
   echo "Website $url seems to be offline."
