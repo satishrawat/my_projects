76bb871fcf630326d09d48d8a9f40b213dc14428
satishrawat <39967722+satishrawat@users.noreply.github.com>
2019-09-20 00:06:40 +0530

    Update smoke.sh

--- smoke.sh
+++ smoke.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-code=$(curl -Is http://35.239.92.56:80 | head -n 1)
-echo 'this is my code' $code
-if [ "code" = "HTTP/1.1 200 OK" ]; then
-  echo "Website $url is online."
-else
-  echo "Website $url seems to be offline."
-fi
+for _ in $(seq 10); do
+  curl -fs http://35.239.92.56:80 > /dev/null
+  if [ "$?" = "0" ]; then
+    echo 'smoke test passed'
+  else
+    kubectl rollout history deployment/test -n ${environment}
+  fi
+done
