fba50b5892ae4b069fe47b9e4d3249e56d5a7245
satishrawat <39967722+satishrawat@users.noreply.github.com>
2019-09-20 00:16:51 +0530

    Update smoke.sh

--- smoke.sh
+++ smoke.sh
@@ -1,8 +1,10 @@
+echo $url
+echo $environment
 for _ in $(seq 10); do
   curl -fs $url > /dev/null
   if [ "$?" = "0" ]; then
     echo 'smoke test passed'
   else
-    kubectl rollout history deployment/test -n ${environment}
+    kubectl rollout history deployment/test -n $environment
   fi
 done
