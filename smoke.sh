542b51816ce1cd9a519c0c77dad5ddf0d5920bf8
satishrawat <39967722+satishrawat@users.noreply.github.com>
2019-09-20 15:09:16 +0530

    Update smoke.sh

--- smoke.sh
+++ smoke.sh
@@ -3,9 +3,11 @@ for _ in $(seq 1); do
   curl -fs $url > /dev/null
   if [ "$?" = "0" ]; then
     echo 'smoke test passed'
-    currentBuild.result = 'FAILURE'
+    
+    
   else
     echo 'its failed performing rollback of deployment'
     kubectl rollout undo deployment/test -n $environment
+    error('Aborting the build.')
   fi
 done
